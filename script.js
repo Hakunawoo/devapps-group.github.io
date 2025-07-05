6c94fbd341c33482a448769b0738da90227970c0
Hakunawoo <91889773+Hakunawoo@users.noreply.github.com>
2025-07-05 15:53:59 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for contacting DEVAPPS GROUP! We'll get back to you soon.");
+});
